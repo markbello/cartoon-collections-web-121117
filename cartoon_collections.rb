a8494ad648af066290276b2af520de6ceac96b73
Mark Bello <mcbello51286@gmail.com>
2017-11-29 17:09:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,10 @@ def long_planeteer_calls(arr)
   arr.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each_with_index{|type|
+    arr.include?(type)
+  }
 end
